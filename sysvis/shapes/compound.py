8b50db06d137075ccfdd83136ec98f5600f7ad8a
signdoubt <upgrade.outdated@gmail.com>
2018-12-18 23:12:50 +0900

    fix arrangement with padding

--- sysvis/shapes/compound.py
+++ sysvis/shapes/compound.py
@@ -44,10 +44,19 @@ class Compound(Shape, ABC):
 
 
 class Zone(Compound):
-    def __init__(self, x: float, y: float, cell_or_groups: List[Shape], **kwargs):
-        width = max([c.width + c.margin.left + c.margin.bottom for c in cell_or_groups])
-        height = reduce(lambda a, b: a + b.height + b.margin.top + b.margin.bottom, [c for c in cell_or_groups], 0)
-        super().__init__(x, y, width, height, **kwargs)
+    def __init__(self, x: float, y: float, cell_or_groups: List[Shape], padding: Gap=None, **kwargs):
+        p = padding or Gap.zero()
+        width = (
+                p.left +
+                max([c.width + c.margin.left + c.margin.right for c in cell_or_groups])
+                + p.right
+        )
+        height = (
+                p.top +
+                reduce(lambda a, b: a + b.margin.top + b.height + b.margin.bottom, [c for c in cell_or_groups], 0)
+                + p.bottom
+        )
+        super().__init__(x, y, width, height, padding=p, **kwargs)
         self.cell_or_groups = cell_or_groups
         self.gap = Gap.gap2(0, 0)
 
@@ -61,37 +70,48 @@ class Zone(Compound):
     def expand_to(self, width: float, height: float) -> 'Zone':
         dw = width - self.width
         dh = height - self.height
-        pad_x, pad_y = self.gap.left, self.gap.top
-        self.gap = Gap.gap2(pad_y + dh / (2 * len(self.cell_or_groups)), pad_x + dw / 2)
+        gx, gy = self.gap.left, self.gap.top
+        self.gap = Gap.gap2(gy + dh / (2 * len(self.cell_or_groups)), gx + dw / 2)
         return self
 
     def fit(self) -> 'Zone':
+        p = self.padding
+        g = self.gap
         x, y = self.x, self.y
-        pad_x, pad_y = self.gap.left, self.gap.top
-        x += pad_x
+        x += p.left + g.left
+        y += p.right
         w = 0
         child_nodes: List[Shape] = []
         for c in self.cell_or_groups:
-            y += pad_y + c.margin.top
+            y += g.top + c.margin.top
             moved = c.move(x, y)
             if isinstance(moved, Zone) or isinstance(moved, Group):
                 moved = moved.fit()
             child_nodes.append(moved)
-            y += moved.height + pad_y + c.margin.bottom
+            y += moved.height + g.bottom + c.margin.bottom
             w = max(moved.width, w)
-        h = y - self.y
-        w = w + pad_x * 2
-        self.width = w
-        self.height = h
+        y += p.bottom
+        x += w + g.right + p.right
+        self.width = x - self.x
+        self.height = y - self.y
         self.cell_or_groups = child_nodes
         return self
 
 
 class Group(Compound):
-    def __init__(self, x: float, y: float, cell_or_zones: List[Shape], **kwargs):
-        width = reduce(lambda a, b: a + b.width + b.margin.left + b.margin.right, [c for c in cell_or_zones], 0)
-        height = max([c.height + c.margin.top + c.margin.bottom for c in cell_or_zones])
-        super().__init__(x, y, width, height, **kwargs)
+    def __init__(self, x: float, y: float, cell_or_zones: List[Shape], padding: Gap=None, **kwargs):
+        p = padding or Gap.zero()
+        width = (
+                p.left +
+                reduce(lambda a, b: a + b.margin.top + b.width + b.margin.bottom, [c for c in cell_or_zones], 0)
+                + p.right
+        )
+        height = (
+                p.top +
+                max([c.height + c.margin.top + c.margin.bottom for c in cell_or_zones])
+                + p.bottom
+        )
+        super().__init__(x, y, width, height, padding=p, **kwargs)
         self.cell_or_zones = cell_or_zones
         self.gap = Gap.gap2(0, 0)
 
@@ -110,22 +130,24 @@ class Group(Compound):
         return self
 
     def fit(self) -> 'Group':
+        p = self.padding
+        g = self.gap
         x, y = self.x, self.y
-        pad_x, pad_y = self.gap.left, self.gap.top
-        y += pad_y
+        y += p.top + g.top
+        x += p.left
         h = 0
         child_nodes: List[Shape] = []
         for c in self.cell_or_zones:
-            x += pad_x + c.margin.left
+            x += g.left + c.margin.left
             moved = c.move(x, y)
             if isinstance(moved, Zone) or isinstance(moved, Group):
                 moved = moved.fit()
             child_nodes.append(moved)
-            x += moved.width + pad_x + c.margin.right
+            x += moved.width + g.right + c.margin.right
             h = max(moved.height, h)
-        w = x - self.x
-        h = h + 2 * pad_y
-        self.width = w
-        self.height = h
+        x += p.right
+        y += h + g.bottom + p.bottom
+        self.height = y - self.y
+        self.width = x - self.x
         self.cell_or_zones = child_nodes
         return self
